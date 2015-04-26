241f14e443190d4133a2bcdf308af23c9971da4c
Chris Rhoda <chrh5521@colorado.edu>
2015-04-26 16:59:54 -0600

    added namesCounter function to header

--- NameList.h
+++ NameList.h
@@ -15,17 +15,18 @@ class nameList{
         nameList();
         ~nameList();
         void orderList(); // order the list by occurrence of names. Most popular to least
+        void bubbleSort();
         void findName(std::string); // input is name to be found
         void nameOccurrence(std::string); // input name, outputs how many times that name has appeared
         void printTopNames(int);  //input how many of the most popular names to print
         void deleteName(std::string);
         void insertName(std::string);
         void printList();
+        int namesCounter(); //counts the number of names in the list
         void printByOcurrence(int);  //print all the names that appear a certain number of times
     protected:
     private:
         Name* head;
-        int test;
 
 
 };
