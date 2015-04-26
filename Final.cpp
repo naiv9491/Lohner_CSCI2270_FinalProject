92f92557db899b211ea98dc1c031c8574b86c492
Chris Rhoda <chrh5521@colorado.edu>
2015-04-25 19:31:01 -0600

    updated skeleton

--- Final.cpp
+++ Final.cpp
@@ -4,27 +4,30 @@
 
 using namespace std;
 
-bool quit = false;
+int main(int argc, char *argv[]){
 
-while(!quit){
+    bool quit = false;
 
-    cout << "======Main Menu======" << endl;
-    cout << "1. Build Trie" << endl;
-    cout << "2. Most popular name starting with a certain letter" << endl;
-    cout << "3. Most popular name in the list" << endl;
-    cout << "4. Quit" << endl;
-    string response;
-    getline(cin, response);
-    if(response == "1"){
+    while(!quit){
 
-    }
-    else if(response == "2"){
+        cout << "======Main Menu======" << endl;
+        cout << "1. Build Trie" << endl;
+        cout << "2. Most popular name starting with a certain letter" << endl;
+        cout << "3. Most popular name in the list" << endl;
+        cout << "4. Quit" << endl;
+        string response;
+        getline(cin, response);
+        if(response == "1"){
 
-    }
-    else if(response == "3"){
+        }
+        else if(response == "2"){
 
-    }
-    else if(response == "4"){
-        quit = true;
+        }
+        else if(response == "3"){
+
+        }
+        else if(response == "4"){
+            quit = true;
+        }
     }
 }
