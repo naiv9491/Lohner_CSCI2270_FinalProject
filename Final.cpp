0b831f9489952d7d7ffdd83d5de8ba1cdaea60b4
Chris Rhoda <chrh5521@colorado.edu>
2015-04-25 18:57:53 -0600

    created menu skeleton

---
+++ Final.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include "Trie.h"
+
+using namespace std;
+
+bool quit = false;
+
+while(!quit){
+
+    cout << "======Main Menu======" << endl;
+    cout << "1. Build Trie" << endl;
+    cout << "2. Most popular name starting with a certain letter" << endl;
+    cout << "3. Most popular name in the list" << endl;
+    cout << "4. Quit" << endl;
+    string response;
+    getline(cin, response);
+    if(response == "1"){
+
+    }
+    else if(response == "2"){
+
+    }
+    else if(response == "3"){
+
+    }
+    else if(response == "4"){
+        quit = true;
+    }
+}
