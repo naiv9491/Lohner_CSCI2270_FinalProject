63b84a40577b61a7455e590a755f35e92578e7b7
Frederik Lohner <freddylohner@gmail.com>
2015-04-25 19:08:28 -0600

    Implemented rest of functions

--- Trie.cpp
+++ Trie.cpp
@@ -68,3 +68,93 @@ void Trie::insert(trie_t *pTrie, char key[])
     // mark last node as leaf (non zero)
     pCrawl->value = pTrie->count;
 }
+
+int Trie::search(trie_t *pTrie, char key[])
+{
+    int level;
+    int length = strlen(key);
+    int index;
+    trie_node_t *pCrawl;
+ 
+    pCrawl = pTrie->root;
+ 
+    for( level = 0; level < length; level++ )
+    {
+        index = INDEX(key[level]);
+ 
+        if( !pCrawl->children[index] )
+        {
+            return 0;
+        }
+ 
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    return (0 != pCrawl && pCrawl->value);
+}
+ 
+int Trie::leafNode(trie_node_t *pNode)
+{
+    return (pNode->value != 0);
+}
+ 
+int Trie::isItFreeNode(trie_node_t *pNode)
+{
+    int i;
+    for(i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if( pNode->children[i] )
+            return 0;
+    }
+ 
+    return 1;
+}
+
+bool Trie::deleteHelper(trie_node_t *pNode, char key[], int level, int len)
+{
+    if( pNode )
+    {
+        // Base case
+        if( level == len )
+        {
+            if( pNode->value )
+            {
+                // Unmark leaf node
+                pNode->value = 0;
+ 
+                // If empty, node to be deleted
+                if( isItFreeNode(pNode) )
+                {
+                    return true;
+                }
+ 
+                return false;
+            }
+        }
+        else // Recursive case
+        {
+            int index = INDEX(key[level]);
+ 
+            if( deleteHelper(pNode->children[index], key, level+1, len) )
+            {
+                // last node marked, delete it
+                FREE(pNode->children[index]);
+ 
+                // recursively climb up, and delete eligible nodes
+                return ( !leafNode(pNode) && isItFreeNode(pNode) );
+            }
+        }
+    }
+ 
+    return false;
+}
+ 
+void Trie::deleteKey(trie_t *pTrie, char key[])
+{
+    int len = strlen(key);
+ 
+    if( len > 0 )
+    {
+        deleteHelper(pTrie->root, key, 0, len);
+    }
+}
